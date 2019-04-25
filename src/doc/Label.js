f359a425deb53b0acdda863da59959842e3b73e9
Ahmad Aidil <aidil@volantis.io>
2019-04-25 13:47:08 +0700

    update sample label component

--- src/doc/Label.js
+++ src/doc/Label.js
@@ -1,11 +1,16 @@
 import styled from 'styled-components'
 
-const Label = styled.div`
+const Label = styled.span`
   display: flex;
   align-items: center;
   svg {
     margin-right: .5rem;
   }
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  user-select: none;
+  box-sizing: border-box;
 `
 
 export default Label
